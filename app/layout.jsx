0d27916b1935cc43eeb10d5d09b9fb940cd4108c
Panuinc <panuwat.ja.forwork@gmail.com>
2025-09-22 09:04:26 +0700

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logoCompany/logoCompany_1.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
